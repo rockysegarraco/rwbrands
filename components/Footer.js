4712880db3678f4b4ed85afbfb6eafd4026d6125
Rocky Segarra <rockysegarra@icloud.com>
2023-08-29 15:31:28 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -54,14 +54,14 @@ export default function Example() {
     <footer className="bg-white border-t mt-16">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-16 lg:px-8">
         <div className="flex justify-center space-x-10 mb-8">
-          <a href="/" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">rw events</span>
             <img
               className="h-8 w-auto"
               src="https://uj8.ad7.myftpupload.com/wp-content/uploads/2023/08/rwlogo.png"
               alt=""
             />
-          </a>
+          </Link>
         </div>
         <nav
           className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
@@ -69,25 +69,25 @@ export default function Example() {
         >
           {navigation.main.map((item) => (
             <div key={item.name} className="pb-6">
-              <a
+              <Link
                 href={item.href}
                 className="text-sm leading-6 text-dark hover:text-gray-900 uppercase tracking-wider"
               >
                 {item.name}
-              </a>
+              </Link>
             </div>
           ))}
         </nav>
         <div className="mt-10 flex justify-center space-x-10">
           {navigation.social.map((item) => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
               className="text-dark hover:text-gray-500"
             >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
-            </a>
+            </Link>
           ))}
         </div>
         <p className="mt-10 text-center text-xs leading-5 text-dark uppercase tracking-wider">
