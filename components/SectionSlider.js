28b1f30e5944b40dd9269959878d72a838c77a70
SagarChavada <sgchavada2013@gmail.com>
2023-09-01 07:28:02 +0530

    Fix slider issue

--- components/SectionSlider.js
+++ components/SectionSlider.js
@@ -4,29 +4,15 @@ import "react-slideshow-image/dist/styles.css";
 
 const SectionSlider = ({ sliderItems }) => {
 
-  const settings = {
-    infinite: true,
-    slidesToShow: 3,
-    autoplay: true,
-    speed: 5000,
-    autoplaySpeed: 0,
-    arrows: false,
-    slidesToScroll: 4,
-    easing: "linear",
-    pauseOnHover: false,
-  };
-
   return (
     <div className="h-screen bg-slate-900 slide-container">
-      <Fade >
+      <Fade duration={2000} infinite={true} easing="linear" >
         {sliderItems.map((item, index) => (
-          <div key={index} className="each-slide-effect each-fade h-screen">
+          <div key={index} className="each-fade h-screen">
             <div
               className="bg-center"
-              style={{
-                backgroundImage: item.url,
-              }}
             >
+              <img src={item.url} alt="" />
               <span className="text-4xl"></span>
             </div>
           </div>
