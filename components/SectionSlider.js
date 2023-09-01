b1fd56dd84e2bb531c0747ee81737c342164ceef
Rocky Segarra <rockysegarra@icloud.com>
2023-09-01 10:19:44 -0400

    Update SectionSlider.js

--- components/SectionSlider.js
+++ components/SectionSlider.js
@@ -3,15 +3,12 @@ import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
 const SectionSlider = ({ sliderItems }) => {
-
   return (
-    <div className="h-screen bg-slate-900 slide-container">
-      <Fade duration={2000} infinite={true} easing="linear" >
+    <div className="bg-dark slide-container">
+      <Fade duration={2000} infinite={true} easing="linear">
         {sliderItems.map((item, index) => (
-          <div key={index} className="each-fade h-screen">
-            <div
-              className="bg-center"
-            >
+          <div key={index} className="each-fade">
+            <div className="bg-center">
               <img src={item.url} alt="" />
               <span className="text-4xl"></span>
             </div>
